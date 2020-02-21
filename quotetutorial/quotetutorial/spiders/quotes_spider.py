952d5548da5c2059c213302ae6ec426ad898481b
DhvaniPatel22 <55349845+DhvaniPatel22@users.noreply.github.com>
2020-02-21 13:08:18 +0530

    Update quotes_spider.py

--- quotetutorial/quotetutorial/spiders/quotes_spider.py
+++ quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -29,57 +29,7 @@ class QuoteSpider(scrapy.Spider):
             yield response.follow(next_page, callback=self.parse)
 
 
-# process = CrawlerProcess(get_project_settings())
 
-# process.crawl('quotes','http://quotes.toscrape.com/page/1/')
-# process.start()
-
-        # for quote in response.css('div.quote'):
-        #name = response.css('.author + a::attr(href)').css('h3.author-title::text').extract()       
-        # for a in response.css('.author + a::attr(href)'):
-        #     name = a.css('h3.author-title::text').extract()
-
-        #     items['name'] = name
-
-        #     yield items
-
-
-    # def parse_author(self, response):
-    #     items = QuotetutorialItem()
-
-    #     name = response.css('h3.author-title::text').extract(),
-    #     birthdate = response.css('.author-born-date::text').extract()
-    #     bio = response.css('.author-description::text').extract()
-
-    #     items['name'] = name
-    #     items['birthdate'] = birthdate
-    #     items['bio'] = bio
-
-    #     yield items
-                
-
-        
-
-        # for href in response.css('.author + a::attr(href)'):
-        #     yield response.follow(href, self.parse_author)
-
-        # for href in response.css('li.next a::attr(href)'):
-        #     yield response.follow(href, self.parse)
-
-    
-
-        
-            #yield {
-                #'text': quote.css('span.text::text').get(),
-                #'author': quote.css('span small::text').get(),
-                #'tags': quote.css('div.tags a.tag::text').getall(),
-                #}
-        
-
- 
-    
-        
-         
 
        
 
